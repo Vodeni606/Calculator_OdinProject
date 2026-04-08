4f613d6c776d4601463c0917947b3c863b2c67b5
Vodeni606 <waterblast606@gmail.com>
2026-04-08 13:23:55 +0200

    added eventlisteners

--- calculator.js
+++ calculator.js
@@ -1,24 +1,33 @@
 const display = document.getElementById('display');
 const buttonContainer = document.getElementById('buttonContainer');
 
-for(let i = 1; i <= 10; i++){
+for(let i = 0; i <= 9; i++){
 
     const button = document.createElement('button');
     button.className = 'calc-button';
-    buttonContainer.appendChild(button);
     button.textContent = i; 
-    
+    buttonContainer.addEventListener('click',function(){
+        display.value += i;
+    });
+    buttonContainer.appendChild(button);
 }       
 
 
 const operationContainer = document.getElementById('operationContainer')
 const operations = ['+','-','/','*']; 
 
-for(let i = 0; i< operations.length;i++){
+for(let i = 0; i < operations.length;i++){
     const button = document.createElement('button');
     button.textContent = operations[i];
     button.className = 'operation-button'
+
+
+    button.addEventListener('click',function(){
+        display.value += operations[i];
+    })
     operationContainer.appendChild(button);
-    
 }
+
+
+
 
